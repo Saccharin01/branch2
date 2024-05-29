3c0b527735e29404aa953cb624a6b4c2ba757a04
Saccharin01 <chousik01@gmail.com>
2024-05-29 12:41:18 +0900

    단위테스트 시작(localhost porting)

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require(`http`)
+const fs = require(`fs`)
 
 let server = http.createServer((req, res)=>{
   console.log(req.method)
   console.log(req.url)
   if(req.method === `GET`){
     if(req.url === `/`){
-      
+      fs.readFile(`index.html`, (err,data)=>{
+        if(err){
+          console.log(`readFile error : `, err)
+        }else{
+          res.write(data)
+          res.end()
+        }
+      })
     };
   };
 });
+let PORT = 8080
+server.listen(PORT)
+console.log(`server open on : http://localhost:${PORT}/`)
